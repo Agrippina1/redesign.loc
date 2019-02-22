9982e422e79251b00568a119ba3d731aa0c400e0
PavelLapin <pavel-lapin@yandex.ru>
2019-02-22 16:42:59 +0300

    Новые модальные формы

--- site/js/js-redesign.js
+++ site/js/js-redesign.js
@@ -27,42 +27,12 @@ var StickyElement = function(node){
 
   $(window).on('scroll', onScroll);
 };
-
 var demo = new StickyElement($('#sticky'));
-
-
-// Форма регистрации
-
-var link = document.querySelector(".input-user");
-if (link != null) {
-    var popup = document.querySelector(".registration-block");
-    var overlay = document.querySelector(".modal-overlay");
-    var close = popup.querySelector(".modal-content-close");
-    link.addEventListener("click", function (event) {
-        event.preventDefault();
-        popup.classList.add("modal-content-show");
-        overlay.classList.add("modal-overlay-show");
-    });
-    close.addEventListener("click", function (event) {
-        event.preventDefault();
-        popup.classList.remove("modal-content-show");
-        overlay.classList.remove("modal-overlay-show");
-    });
-    window.addEventListener("keydown", function (event) {
-        if (event.keyCode === 27) {
-            if (popup.classList.contains("modal-content-show")) {
-                popup.classList.remove("modal-content-show");
-                overlay.classList.remove("modal-overlay-show");
-            }
-        }
-    });
-}
-
 
 // Модальные окна и меню
 $(document).ready(function() {
     var overlay = $('#overlay');            // подложка
-    var open_modal = $('.open_modal');      // все ссылки oткрывaющие oкнa
+    var open_modal = $('.modal_open');      // все ссылки oткрывaющие oкнa
     var close = $('.modal_close, #overlay');// чтo зaкрывaет мoдaльнoе oкнo
     var modal = $('.modal_div');            // скрытые мoдaльные oкнa
 
@@ -74,11 +44,10 @@ $(document).ready(function() {
         event.preventDefault();
         var div = $(this).attr('href');
         overlay.fadeIn(0,
-            function() {
-                $(div).css('display', 'block').animate({opacity: 1, top: '50%'}, 200);
+            function () {
+                $(div).css('display', 'block').animate({opacity: 1}, 200);
                 $("html,body").animate({scrollTop: $("header").offset().top}, "slow");
             });
+
     });
-
-
 });
