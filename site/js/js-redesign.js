24762b907c1b22be03d397d5cf43c1eda6f6b7a0
PavelLapin <pavel-lapin@yandex.ru>
2019-03-25 14:54:46 +0300

    Обработка checkbox js

--- site/js/js-redesign.js
+++ site/js/js-redesign.js
@@ -56,3 +56,15 @@ $(document).ready(function() {
         $("html,body").animate({scrollTop: $("header").offset().top}, "slow");
     });
 });
+
+function changeCheckbox(id_element) {
+    let item = document.getElementById(id_element);
+    switch(item.getAttribute('aria-checked')) {
+        case "true":
+            item.setAttribute('aria-checked', "false");
+            break;
+        case "false":
+            item.setAttribute('aria-checked', "true");
+            break;
+    }
+}
