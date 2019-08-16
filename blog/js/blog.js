21944e92c1fb263d506f30d10bd06765747e7614
PavelLapin <pavel-lapin@yandex.ru>
2019-08-16 15:03:01 +0300

    тесты

--- blog/js/blog.js
+++ blog/js/blog.js
@@ -1,27 +1,45 @@
+$(document).ready(function()
+{
+	$( "#tabs" ).tabs({
+		beforeLoad: function( event, ui ) {
+			ui.jqXHR.fail(function() {
+				ui.panel.html(
+					"РќРµС‚ СЃРѕРµРґРёРЅРµРЅРёСЏ СЃ СЃРµСЂРІРµСЂРѕРј, РїРѕРїСЂРѕР±СѓР№С‚Рµ Р·Р°Р№С‚Рё РїРѕР·Р¶Рµ." );
+			});
+		}
+	});
+	// $("#tabs").tabs({
+	// 	ajaxOptions: {
+	// 		error: function(xhr, status, index, anchor) {
+	// 			$(anchor.hash).html("РќРµС‚ СЃРѕРµРґРёРЅРµРЅРёСЏ СЃ СЃРµСЂРІРµСЂРѕРј, РїРѕРїСЂРѕР±СѓР№С‚Рµ Р·Р°Р№С‚Рё РїРѕР·Р¶Рµ.");
+	// 		}
+	// 	}
+	// });
+});
 function ShowLoadBar(mesto)
 {
-	$('<div align="center"><br><img src="pics/loadbar.gif" width="128" height="15" border="0" alt="загрузка..." title="загрузка..."><br></div>').appendTo(mesto);				
+	$('<div align="center"><br><img src="pics/loadbar.gif" width="128" height="15" border="0" alt="Р·Р°РіСЂСѓР·РєР°..." title="Р·Р°РіСЂСѓР·РєР°..."><br></div>').appendTo(mesto);				
 }
 function ShowLoadBarMini(mesto)
 {
-	$(mesto).html('<img src="pics/loader_f.gif" width="15" height="15" border="0" alt="ждите..." title="ждите...">');				
+	$(mesto).html('<img src="pics/loader_f.gif" width="15" height="15" border="0" alt="Р¶РґРёС‚Рµ..." title="Р¶РґРёС‚Рµ...">');				
 }
 function ShowLoadBarMicro(mesto)
 {
-	$(mesto).html('<img src="pics/loader_m.gif" width="16" height="11" border="0" alt="ждите..." title="ждите...">');				
+	$(mesto).html('<img src="pics/loader_m.gif" width="16" height="11" border="0" alt="Р¶РґРёС‚Рµ..." title="Р¶РґРёС‚Рµ...">');				
 }
 function ShowForm(param)
 {
         var id = param.id;
         var ignor = param.ignor || false;
 
-        if (ignor) var form_ans = "<div style='background-color: #FDE5DD; border: 1px solid #ff0000; margin-bottom: 2em; padding: 1em;'>Извините, Вы внесены в игнор-лист автора дневника и не можете писать ответы.</div>";
+        if (ignor) var form_ans = "<div style='background-color: #FDE5DD; border: 1px solid #ff0000; margin-bottom: 2em; padding: 1em;'>РР·РІРёРЅРёС‚Рµ, Р’С‹ РІРЅРµСЃРµРЅС‹ РІ РёРіРЅРѕСЂ-Р»РёСЃС‚ Р°РІС‚РѕСЂР° РґРЅРµРІРЅРёРєР° Рё РЅРµ РјРѕР¶РµС‚Рµ РїРёСЃР°С‚СЊ РѕС‚РІРµС‚С‹.</div>";
         else {
-            var form_ans = "<fieldset style='width: 710px; border: 1px solid #DFDFDF;'><legend>Текст ответа</legend>"
+            var form_ans = "<fieldset style='width: 710px; border: 1px solid #DFDFDF;'><legend>РўРµРєСЃС‚ РѕС‚РІРµС‚Р°</legend>"
                 + "<form id='answer_form'>"
                 + "<textarea id='ans_text' cols='50' rows='5' name='ans_text'></textarea>"
                 + "<input type='hidden' name='id' value='" + id + "'>"
-                + "<div><input type='button' value='Отправить' class='form_button' onClick='submitAnswer(" + id + ")'></div>"
+                + "<div><input type='button' value='РћС‚РїСЂР°РІРёС‚СЊ' class='form_button' onClick='submitAnswer(" + id + ")'></div>"
                 + "</form>"
                 + "";
         }
@@ -92,7 +110,7 @@ function ShowAnswer(id, page, comment)
 			$("#sap_"+id).html(data);
 			$("#post_ba_"+id).hide();
 			if (comment == 'write') $("#post_bf_"+id).show();
-			else $("#post_bсс_"+id).show();
+			else $("#post_bСЃСЃ_"+id).show();
 
 			$("#post_rfbtn_"+id).show();			
 		}
@@ -100,14 +118,14 @@ function ShowAnswer(id, page, comment)
 	return false;
 
 }
-function MadeOnRake (id) { return $("#rbp_"+id).html('<a href="#" onclick="return RatingPost(\''+id+'\',\'good\');"><img src="pics/plus.gif" width="16" height="12" border="0" alt="плюс" title="плюс" /></a><a href="#" onclick="return RatingPost(\''+id+'\',\'bad\');"><img src="pics/minus.gif" width="16" height="12" border="0" alt="минус" title="минус"/></a>')};
+function MadeOnRake (id) { return $("#rbp_"+id).html('<a href="#" onclick="return RatingPost(\''+id+'\',\'good\');"><img src="pics/plus.gif" width="16" height="12" border="0" alt="РїР»СЋСЃ" title="РїР»СЋСЃ" /></a><a href="#" onclick="return RatingPost(\''+id+'\',\'bad\');"><img src="pics/minus.gif" width="16" height="12" border="0" alt="РјРёРЅСѓСЃ" title="РјРёРЅСѓСЃ"/></a>')};
 
 function SubansToggle(id,mode)
 {
 	$(document).ready(function()
 	{					 	
-		if (mode=='yes') $("#ans_subp_"+id).html('<a href="#" onclick="return Subans(\''+id+'\',\'no\');"><img src="pics/com_s.png" width="16" height="16" border="0" alt="уведомлять об ответах" title="уведомлять об ответах" /></a>');
-		else             $("#ans_subp_"+id).html('<a href="#" onclick="return Subans(\''+id+'\',\'yes\');"><img src="pics/com_u.png" width="16" height="16" border="0" alt="не сообщать об ответах" title="не сообщать об ответах" /></a>');
+		if (mode=='yes') $("#ans_subp_"+id).html('<a href="#" onclick="return Subans(\''+id+'\',\'no\');"><img src="pics/com_s.png" width="16" height="16" border="0" alt="СѓРІРµРґРѕРјР»СЏС‚СЊ РѕР± РѕС‚РІРµС‚Р°С…" title="СѓРІРµРґРѕРјР»СЏС‚СЊ РѕР± РѕС‚РІРµС‚Р°С…" /></a>');
+		else             $("#ans_subp_"+id).html('<a href="#" onclick="return Subans(\''+id+'\',\'yes\');"><img src="pics/com_u.png" width="16" height="16" border="0" alt="РЅРµ СЃРѕРѕР±С‰Р°С‚СЊ РѕР± РѕС‚РІРµС‚Р°С…" title="РЅРµ СЃРѕРѕР±С‰Р°С‚СЊ РѕР± РѕС‚РІРµС‚Р°С…" /></a>');
 	});
 }
 function Subans(id,mode)
@@ -134,7 +152,7 @@ function ShowPost(id,comment)
 			$("#sfp_"+id).html(data);
 			$("#post_bp_"+id).hide();
             if (comment == 'write') $("#post_bf_"+id).show();
-            else $("#post_bсс_"+id).show();
+            else $("#post_bСЃСЃ_"+id).show();
             $("#post_rfbtn_"+id).show();
 			
 			MadeOnRake(id);					 
@@ -144,7 +162,7 @@ function ShowPost(id,comment)
 }
 function DelAns(id)
 {
-	if (confirm("Вы действительно хотите удалить сообщение?")) 
+	if (confirm("Р’С‹ РґРµР№СЃС‚РІРёС‚РµР»СЊРЅРѕ С…РѕС‚РёС‚Рµ СѓРґР°Р»РёС‚СЊ СЃРѕРѕР±С‰РµРЅРёРµ?")) 
 	{
 		$(document).ready(function(){
 			 	
@@ -162,7 +180,7 @@ function DelAns(id)
 }
 function DelPost(id)
 {
-	if (confirm("Вы действительно хотите удалить пост и все ответы к нему?")) 
+	if (confirm("Р’С‹ РґРµР№СЃС‚РІРёС‚РµР»СЊРЅРѕ С…РѕС‚РёС‚Рµ СѓРґР°Р»РёС‚СЊ РїРѕСЃС‚ Рё РІСЃРµ РѕС‚РІРµС‚С‹ Рє РЅРµРјСѓ?")) 
 	{
 		$(document).ready(function(){
 			 	
